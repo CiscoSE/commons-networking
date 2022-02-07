f89c2170d0c15d879456b83079666d1ce7f6cde4
liran2000 <liran2000@gmail.com>
2022-02-07 11:56:50 +0200

    Add support for keep-alive mechanism handling

--- commons-networking/src/main/java/com/cisco/commons/networking/SSEClient.java
+++ commons-networking/src/main/java/com/cisco/commons/networking/SSEClient.java
@@ -6,7 +6,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -16,7 +19,9 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -25,6 +30,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.net.ssl.SSLContext;
 
@@ -34,11 +40,12 @@ import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * SSE (Server-sent events) client.
- * Server-Sent Events (<a href="https://datatracker.ietf.org/doc/html/rfc8895">SSE</a>) is a <a href="https://en.wikipedia.org/wiki/Push_technology">server push</a> technology 
+ * Server-Sent Events (SSE) is a <a href="https://en.wikipedia.org/wiki/Push_technology">server push</a> technology 
  * enabling a client to receive automatic updates from a server via HTTP connection. <br/>
  * The Server-Sent Events EventSource API is standardized as part of 
  * <a href="https://html.spec.whatwg.org/multipage/server-sent-events.html#the-eventsource-interface">HTML5</a>. <br/>
@@ -53,6 +60,10 @@ import lombok.extern.slf4j.Slf4j;
  * This is translating to: System.setProperty("jdk.internal.httpclient.disableHostnameVerification", "true"); 
  * This is not best practice as it is risky and sets a global system property, it is better to work with a trusted host.
  * <br/>
+ * When useKeepAliveMechanismIfReceived is used, and keep-alive / comment messages are received, it is 
+ * handled by logic which will disconnect / reconnect to the stream when no messages are received for some
+ * time by a calculated interval. When it is enabled, any comment message will be treated as a keep-alive 
+ * message per the SSE protocol specifications, as specific keep-alive messages are not well-defined.
  * <br/>
  * Copyright 2021 Cisco Systems
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -71,9 +82,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SSEClient {
 
-    private static final long DEFAULT_RECONNECT_SAMPLING_TIME_MILLIS = 60L * 1000L;
+	private static final long DEFAULT_RECONNECT_SAMPLING_TIME_MILLIS = 60L * 1000L;
     private static final long DEFAULT_CONNECTIVITY_REFRESH_DELAY = 24;
     private static final TimeUnit DEFAULT_CONNECTIVITY_REFRESH_DELAY_TIME_UNIT = TimeUnit.HOURS;
+    private static final int DEFAULT_CONNECTIVITY_CHECK_INTERVAL_SECONDS = 2 * 60;
+    private static final int DEFAULT_MIN_CONNECTIVITY_THRESHOLD_SECONDS = 2 * 60;
+	private static final int KEEP_ALIVE_SAMPLING_MESSAGES_COUNT = 4;
+	protected static final String CONNECTIVITY_CHECK = "connectivity_check";
     
 	private String url;
 	private Map<String, String> headerParams;
@@ -81,6 +96,7 @@ public class SSEClient {
 	private long reconnectSamplingTimeMillis = DEFAULT_RECONNECT_SAMPLING_TIME_MILLIS;
 	private long connectivityRefreshDelay = DEFAULT_CONNECTIVITY_REFRESH_DELAY;
 	private TimeUnit connectivityRefreshDelayTimeUnit = DEFAULT_CONNECTIVITY_REFRESH_DELAY_TIME_UNIT;
+	private String connectivityCheckUrl;
 	
     private InputStream inputStream;
     private SSLContext sslContext;
@@ -88,7 +104,20 @@ public class SSEClient {
     private AtomicBoolean shouldRun = new AtomicBoolean(true);
     private AtomicBoolean shouldSkipSleep = new AtomicBoolean(false);
     private String threadName;
+    
+    @Getter
     private SubscribeStatus status = SubscribeStatus.NOT_STARTED;
+    
+    private AtomicBoolean isConnected = new AtomicBoolean(true);
+    private boolean useConnectivityCheck;
+    private boolean useKeepAliveMechanismIfReceived;
+    private Integer connectivityCheckIntervalSeconds = DEFAULT_CONNECTIVITY_CHECK_INTERVAL_SECONDS;
+    private Integer minConnectivityThresholdSeconds = DEFAULT_MIN_CONNECTIVITY_THRESHOLD_SECONDS;
+	private long connectivityThresholdMillis;
+	private AtomicBoolean keepAliveReceivedInCurrentRequest = new AtomicBoolean(false);
+    private List<Long> firstKeepAliveMessageTimes = new ArrayList<>();    
+    private long lastReceivedMessageTime = 0L;
+    private AtomicLong reconnectionsCount = new AtomicLong(0L);
     
     private ExecutorService pool;
     private ScheduledExecutorService connectivityRefreshPoolScheduler;
@@ -100,7 +129,9 @@ public class SSEClient {
     @Builder
     public SSEClient(String url, Map<String, String> headerParams, EventHandler eventHandler,
     		Long reconnectSamplingTimeMillis, Long connectivityRefreshDelay,
-    		TimeUnit connectivityRefreshDelayTimeUnit, boolean setDisableHostnameVerificationSystemProperty) {
+    		TimeUnit connectivityRefreshDelayTimeUnit, boolean setDisableHostnameVerificationSystemProperty,
+    		boolean useKeepAliveMechanismIfReceived, boolean useConnectivityCheck,
+    		Integer connectivityCheckIntervalSeconds, Integer minConnectivityThresholdSeconds) {
 		super();
 		if (url == null) {
 			throw new IllegalArgumentException("url cannot be null");
@@ -129,11 +160,31 @@ public class SSEClient {
 			this.connectivityRefreshDelayTimeUnit = connectivityRefreshDelayTimeUnit;
 		}
 		this.url = url;
+		try {
+			URL urlObject = new URL(new URL(url), "/");
+			connectivityCheckUrl = urlObject.toString() + CONNECTIVITY_CHECK;
+			log.info("connectivityCheckUrl: {}", connectivityCheckUrl);
+		} catch (MalformedURLException e) {
+			throw new IllegalArgumentException("invalid URL: " + url + ": " + e.getMessage());
+		}
+		
 		this.headerParams = headerParams;
 		if (this.headerParams == null) {
 			this.headerParams = new HashMap<>();
 		}
 		this.eventHandler = eventHandler;
+		this.useKeepAliveMechanismIfReceived = useKeepAliveMechanismIfReceived;
+		this.useConnectivityCheck = useConnectivityCheck;
+		if (useKeepAliveMechanismIfReceived) {
+			log.info("useKeepAliveMechanismIfReceived is true, setting useConnectivityCheck to true.");
+			this.useConnectivityCheck = true;
+		}
+		if (connectivityCheckIntervalSeconds != null) {
+			this.connectivityCheckIntervalSeconds = connectivityCheckIntervalSeconds;
+		}
+		if (minConnectivityThresholdSeconds != null) {
+			this.minConnectivityThresholdSeconds = minConnectivityThresholdSeconds;
+		}
 		this.pool = Executors.newSingleThreadExecutor();
 		this.connectivityRefreshPoolScheduler = Executors.newScheduledThreadPool(1);
 		try {
@@ -191,7 +242,10 @@ public class SSEClient {
                 .uri(new URI(url))
                 .version(HttpClient.Version.HTTP_2)
                 .GET()
-                .header("Accept", "text/event-stream");
+                .timeout(Duration.ofSeconds(30))
+                .header("Accept", "text/event-stream")
+                .header("Cache-Control", "no-cache")
+				;
 
             Set<Entry<String, String>> customHeaderParams = headerParams.entrySet();
 			for (Entry<String, String> headerParam : customHeaderParams) {
@@ -200,6 +254,7 @@ public class SSEClient {
 			HttpRequest request = requestBuilder.build();
             
             HttpResponse<InputStream> response = HttpClient.newBuilder().sslContext(sslContext)
+        		.connectTimeout(Duration.ofSeconds(30))
                 .build()
                 .send(request, BodyHandlers.ofInputStream());
             
@@ -237,19 +292,42 @@ public class SSEClient {
          * Separate notifications following <a href="https://datatracker.ietf.org/doc/html/rfc8895">SSE</a> (server-sent-events) protocol:
          * - A message text goes after 'data: ', the space after the colon is optional.
          * - A message is terminated by a blank line (two line terminators in a row).
+         * 
+         * A Sample SSE Stream:
+         * event: start
+	     * id: 1
+	     * data: hello there
+	     * :keepalive
+		 * 
+	     * event: middle
+	     * id: 2
+	     * data: let's chat some more ...
+	     * data: and more and more and ...
+	     * :keepalive
+	     * 
+	     * event: end
+	     * id: 3
+	     * data: goodbye
          */
     	
         log.info("Handling response.");
+        keepAliveReceivedInCurrentRequest.set(false);
+        connectivityThresholdMillis = 0;
         try (BufferedInputStream in = IOUtils.buffer(inputStream)) {
         	try (BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))) {
         		String line = null;
         		StringBuilder messageBuilder = new StringBuilder();
         		while((line = reader.readLine()) != null) {
+        			lastReceivedMessageTime = System.currentTimeMillis();
         			String content = line;
         			if (content.startsWith("data:")) {
         				messageBuilder.append(content.substring(5));
-    				} else {
-    					log.debug("Ignoring non-data line: {}", content);
+    				} else if (useKeepAliveMechanismIfReceived && content.startsWith(":")) {
+                    	log.debug("Got keeplive / comment message: {}", content);
+                    	keepAliveReceivedInCurrentRequest.set(true);
+                    	processKeepAliveMessage();
+                    } else {
+    					log.debug("Got non-data line: {}", content);
     				}
 					if (line.trim().isEmpty() && messageBuilder.length() > 0) {
 						String message = messageBuilder.toString();
@@ -257,6 +335,9 @@ public class SSEClient {
         				messageBuilder = new StringBuilder();
                     }
         	    }
+        		if (messageBuilder.length() > 0) {
+                	log.info("Non-processed data: {}", messageBuilder);
+                }
         	}
         } catch (Exception e) {
             String errorMessage = "Could not handle response: " + e.getMessage();
@@ -297,17 +378,7 @@ public class SSEClient {
     
     public void start() {
     	log.info("Subscribing changes from " + url);
-        Runnable connectivityRefreshTask = () -> {
-            try {
-                log.info("Stopping current request, for refreshing connectivity with new request.");
-                stopCurrentRequest();
-            } catch (Exception e) {
-                log.info("Got error at connectivityRefreshTask: " + e.getMessage());
-            }
-        };
-        log.info("Scheduling connectity refresh task.");
-        scheduleConnectivityRefreshTask(connectivityRefreshTask);
-        
+    	scheduleConnectivityTasks();
         pool.execute(() -> {
         	try {
 				getChanges();
@@ -318,14 +389,145 @@ public class SSEClient {
         
         log.info("done.");
     }
+    
+    protected void scheduleConnectivityTasks() {
+    	Runnable connectivityRefreshTask = () -> {
+            try {
+                log.info("Stopping current request, for refreshing connectivity with new request.");
+                stopCurrentRequest();
+            } catch (Exception e) {
+                log.info("Got error at connectivityRefreshTask: " + e.getMessage());
+            }
+        };
+        log.info("Scheduling connectity refresh task.");
+    	scheduleConnectivityRefreshTask(connectivityRefreshTask);
+    	if (useConnectivityCheck) {
+	    	Runnable connectivityCheckTask = () -> {
+	            try {
+	            	if (!isSubscribedSuccessfully()) {
+	            		log.debug("Status is not subscribed successfully. Skipping connectivity check.");
+	            		return;
+	            	}
+	            	boolean wasConnected = isConnected.get();
+	                boolean isCurrentlyConnected = isCurrentlyConnected();
+	                log.debug("connectivityCheckTask - isCurrentlyConnected: {}, wasConnected: {}, keepAliveReceived: {}, connectivityThresholdMillis: {}", 
+	            		isCurrentlyConnected, wasConnected, keepAliveReceivedInCurrentRequest, connectivityThresholdMillis);
+	                boolean shouldReconnect = false;
+	                if (keepAliveReceivedInCurrentRequest.get() && connectivityThresholdMillis > 0) {
+	                	if (!isCurrentlyConnected) {
+	                		log.info("time from last received message not valid.");
+	                		shouldReconnect = true;
+	                	}
+	                } else if (!wasConnected && isCurrentlyConnected) {
+	                	log.info("Previous status was disconnected, and now it is connected.");
+	                }
+	                if (!isCurrentlyConnected) {
+	                	shouldReconnect = true;
+	                }
+	                if (shouldReconnect) {
+	                	log.info("Stopping current request, for refreshing connectivity with new request.");
+	                	keepAliveReceivedInCurrentRequest.set(false);
+	            		reconnectionsCount.incrementAndGet();
+	                	stopCurrentRequest();
+	                }
+	                isConnected.set(isCurrentlyConnected);
+	            } catch (Exception e) {
+	                log.info("Got error at connectivityRefreshTask: " + e.getMessage());
+	            }
+	        };
+	    	log.info("Scheduling connectity check task every {} seconds.", connectivityCheckIntervalSeconds);
+	        connectivityRefreshPoolScheduler.scheduleWithFixedDelay(connectivityCheckTask ,
+	        		connectivityCheckIntervalSeconds, connectivityCheckIntervalSeconds, TimeUnit.SECONDS);
+    	}
+    }
+    
+    private boolean isCurrentlyConnected() {
+		try {
+			if (!isLastReceivedMessageTimeValid()) {
+				return false;
+			}
+			log.debug("Checking via connectivity check URL request: {}", connectivityCheckUrl);
+		    HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+                .uri(new URI(connectivityCheckUrl))
+                .GET()
+                .timeout(Duration.ofSeconds(3));
+		    Set<Entry<String, String>> customHeaderParams = headerParams.entrySet();
+			for (Entry<String, String> headerParam : customHeaderParams) {
+				requestBuilder.header(headerParam.getKey(), headerParam.getValue());
+			}
+			HttpRequest request = requestBuilder.build();
+            HttpResponse<InputStream> response = HttpClient.newBuilder().sslContext(sslContext)
+        		.connectTimeout(Duration.ofSeconds(3))
+                .build()
+                .send(request, BodyHandlers.ofInputStream());
+			log.debug("connectivityCheck response status: {}", response.statusCode());
+			return true;
+		} catch (ConnectException e) {
+			log.info("isCurrentlyConnected - ConnectException: " + e.getMessage());
+			return false;
+		} catch (Exception e) {
+			log.error("Errror in isCurrentlyConnected: " + e.getClass() + ", " + e.getMessage(), e);
+			return false;
+		}
+	}
+    
+    private boolean isLastReceivedMessageTimeValid() {
+		if (keepAliveReceivedInCurrentRequest.get() && connectivityThresholdMillis > 0) {
+			long timeFromLastReceivedMessageMs = System.currentTimeMillis() - lastReceivedMessageTime;
+			if (timeFromLastReceivedMessageMs > connectivityThresholdMillis) {
+				log.warn("time from last received message ( {} ms) is more than the threshold ( {} ). Not connected.", timeFromLastReceivedMessageMs, connectivityThresholdMillis );
+				return false;
+			}
+		}
+		return true;
+	}
 
     protected void scheduleConnectivityRefreshTask(Runnable connectivityRefreshTask) {
         connectivityRefreshPoolScheduler.scheduleWithFixedDelay(connectivityRefreshTask , connectivityRefreshDelay, connectivityRefreshDelay, connectivityRefreshDelayTimeUnit);
     }
+    
+    private void processKeepAliveMessage() {
+		if (firstKeepAliveMessageTimes.size() < KEEP_ALIVE_SAMPLING_MESSAGES_COUNT) {
+			firstKeepAliveMessageTimes.add(lastReceivedMessageTime);
+			int firstKeepAliveMessageTimesSize = firstKeepAliveMessageTimes.size();
+			if (firstKeepAliveMessageTimesSize == 1) {
+				log.info("First keep-alive received from current request-response.");
+			}
+			if (firstKeepAliveMessageTimesSize == KEEP_ALIVE_SAMPLING_MESSAGES_COUNT) {
+				log.info("Received enough keep-alive messages for calculating interval.");
+				setConnectivityThresholdByKeepAliveIntervalAverage();
+			}
+		}
+	}
 
-    protected SubscribeStatus getStatus() {
-        return status;
-    }
+	/*
+	 * We calculate the keep-alive messages interval by average, since it depends on NSO configuration.
+	 */
+	private void setConnectivityThresholdByKeepAliveIntervalAverage() {
+		int keepAliveIntervalsSum = 0;
+		int end = firstKeepAliveMessageTimes.size() - 1;
+		for (int i = 0; i < end; i++) {
+			long keepAliveInterval = firstKeepAliveMessageTimes.get(i+1)
+				- firstKeepAliveMessageTimes.get(i);
+			keepAliveIntervalsSum += keepAliveInterval;
+		}
+		log.debug("keepAliveIntervalsSum: {}", keepAliveIntervalsSum);
+		int keepAliveIntervalsAverageSeconds = (int) Math.rint((double)keepAliveIntervalsSum / (double)(end) / 1000.0);
+		log.info("keepAliveIntervalsAverageSeconds: {}", keepAliveIntervalsAverageSeconds);
+		long keepAliveIntervalsAverageMultiplier = 4L;
+		connectivityThresholdMillis = keepAliveIntervalsAverageSeconds * keepAliveIntervalsAverageMultiplier * 1000L;
+		long minConnectivityThresholdMillis = minConnectivityThresholdSeconds * 1000L;
+		if (connectivityThresholdMillis < minConnectivityThresholdMillis) {
+			connectivityThresholdMillis = minConnectivityThresholdMillis;
+			log.info("calculated connectivity threshold average ( {} ms ) is lower than the minimum ( {} ms ). Using the minimum value.", keepAliveIntervalsAverageSeconds, minConnectivityThresholdMillis );
+		} else {
+			log.info("Setting connectivity threshold by received keep-alive messages to {} times than the average: {} ms.", keepAliveIntervalsAverageMultiplier, connectivityThresholdMillis);
+		}
+	}
+	
+	public long getReconnectionsCount() {
+		return reconnectionsCount.get();
+	}
     
     public void shutdown() {
         try {
--- commons-networking/src/test/java/com/cisco/commons/networking/SSEClientTest.java
+++ commons-networking/src/test/java/com/cisco/commons/networking/SSEClientTest.java
@@ -8,6 +8,7 @@ import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 import org.junit.Test;
 
@@ -40,9 +41,10 @@ public class SSEClientTest {
 	
 	private static final String EVENT1 = "event1";
 	private static final String EVENT2 = "event2";
+	private static final String KEEPALIVE_MESSAGE = ":keepalive\n";
 	
 	@Test
-	public void sSEClientTestEmbeddedServer() throws Exception {
+	public void sseClientTestEmbeddedServer() throws Exception {
 		String api = "/api";
 		HttpServer httpServer = createHttpServer(0, api);
 		log.info("Starting server");
@@ -57,7 +59,7 @@ public class SSEClientTest {
 				.build();
 			log.info("Starting SSE Client");
 			sseClient.start();
-			Thread.sleep(2 * 1000);
+			sleepQuitely(2 * 1000);
 			assertEquals(SubscribeStatus.SUCCESS, sseClient.getStatus());
 			assertEquals(2, events.size());
 			assertEquals(EVENT2, events.get(1));
@@ -71,8 +73,8 @@ public class SSEClientTest {
 	}
 	
 	@Test
-	public void sSEClientTestEmbeddedServerReconnect() throws Exception {
-		log.info("sSEClientTestEmbeddedServerReconnect - begin");
+	public void sseClientTestEmbeddedServerReconnectDueToServerStopped() throws Exception {
+		log.info("sseClientTestEmbeddedServerReconnectDueToServerStopped - begin");
 		String api = "/api";
 		HttpServer httpServer = createHttpServer(0, api);
 		log.info("Starting server");
@@ -87,7 +89,7 @@ public class SSEClientTest {
 			.build();
 		log.info("Starting SSE Client");
 		sseClient.start();
-		Thread.sleep(2 * 1000);
+		sleepQuitely(2 * 1000);
 		assertEquals(SubscribeStatus.SUCCESS, sseClient.getStatus());
 		assertEquals(2, events.size());
 		assertEquals(EVENT2, events.get(1));
@@ -96,14 +98,14 @@ public class SSEClientTest {
 		log.info("Stopping server");
 		httpServer.stop(0);
 		
-		Thread.sleep(1000);
+		sleepQuitely(1000);
 		assertEquals(SubscribeStatus.RECONNECTING, sseClient.getStatus());
 		
 		log.info("Starting server");
 		httpServer = createHttpServer(port, api);
 		httpServer.start();
 		
-		Thread.sleep(3 * 1000);
+		sleepQuitely(3000);
 		
 		assertEquals(4, events.size());
 		assertEquals(EVENT2, events.get(3));
@@ -118,10 +120,59 @@ public class SSEClientTest {
 		
 		log.info("sSEClientTestEmbeddedServerReconnect - end");
 	}
+	
+	@Test
+	public void sseClientTestEmbeddedServerReconnectDueToMissingKeepAlive() throws Exception {
+		log.info("sseClientTestEmbeddedServerReconnectDueToMissingKeepAlive - begin");
+		String api = "/api";
+		HttpServer httpServer = createHttpServer(0, api, true);
+		log.info("Starting server");
+		httpServer.start();
 
+		List<String> events = new LinkedList<>();
+		int port = httpServer.getAddress().getPort();
+		log.info("Listening to port: {}", port);
+		String url = "http://localhost:" + port + api;
+		EventHandler eventHandler = eventText -> { events.add(eventText); };
+		SSEClient sseClient = SSEClient.builder().url(url).eventHandler(eventHandler)
+			.reconnectSamplingTimeMillis(1000L)
+			.useKeepAliveMechanismIfReceived(true)
+			.connectivityCheckIntervalSeconds(1).minConnectivityThresholdSeconds(1)
+			.build();
+		log.info("Starting SSE Client");
+		sseClient.start();
+		sleepQuitely(2 * 1000);
+		assertEquals(SubscribeStatus.SUCCESS, sseClient.getStatus());
+		assertEquals(2, events.size());
+		assertEquals(EVENT2, events.get(1));
+		
+		log.info("Simulating keep-alive not sent.");
+		sleepQuitely(8 * 1000);
+		assertEquals(1, sseClient.getReconnectionsCount());
+		assertEquals(SubscribeStatus.SUCCESS, sseClient.getStatus());
+		
+		log.info("Stopping SSE Client");
+		sseClient.shutdown();
+		
+		assertEquals(SubscribeStatus.STOPPED, sseClient.getStatus());
+		
+		log.info("Stopping server");
+		httpServer.stop(0);
+		
+		log.info("sSEClientTestEmbeddedServerReconnect - end");
+	}
+	
 	private HttpServer createHttpServer(int port, String api) throws IOException {
+		return createHttpServer(port, api, false);
+	}
+
+	private HttpServer createHttpServer(int port, String api, boolean sendKeepAliveMessagesBlocking) throws IOException {
 		HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+		httpServer.setExecutor(Executors.newCachedThreadPool());
 		httpServer.createContext(api, new HttpHandler() {
+			
+			private boolean returnedResponse = false;
+			
 			@Override
 			public void handle(HttpExchange exchange) throws IOException {
 				byte[] response = ("data: " + EVENT1 + "\n\n").getBytes(StandardCharsets.UTF_8);
@@ -131,16 +182,46 @@ public class SSEClientTest {
 				response = ("error: " + "error_message" + "\n\n").getBytes(StandardCharsets.UTF_8);
 				exchange.getResponseBody().write(response);
 				exchange.getResponseBody().flush();
-				response = (": " + "comment" + "\n\n").getBytes(StandardCharsets.UTF_8);
-				exchange.getResponseBody().write(response);
-				exchange.getResponseBody().flush();
+				sendKeepAlive(exchange);
 				sleepQuitely(1000);
 				response = ("data: " + EVENT2 + "\n\n").getBytes(StandardCharsets.UTF_8);
 				exchange.getResponseBody().write(response);
 				exchange.getResponseBody().flush();
 				
+				if (sendKeepAliveMessagesBlocking) {
+					sendKeepAliveMessagesBlocking(exchange, 3);
+				}
+				
 				// Intentionally not closing, imitate a non-ending stream response.
 //				exchange.close();
+			}
+
+			private void sendKeepAlive(HttpExchange exchange) throws IOException {
+				byte[] response;
+				response = (": " + "keepalive" + "\n\n").getBytes(StandardCharsets.UTF_8);
+				exchange.getResponseBody().write(response);
+				exchange.getResponseBody().flush();
+			}
+			
+			private void sendKeepAliveMessagesBlocking(HttpExchange exchange, int seconds) throws IOException {
+				String changes;
+				for (int i = 0; i < seconds; i++) {
+					changes = KEEPALIVE_MESSAGE;
+					exchange.getResponseBody().write(changes.getBytes(StandardCharsets.UTF_8));
+					exchange.getResponseBody().flush();
+					sleepQuitely(1000);
+				}
+			}
+		});
+		httpServer.createContext("/" + SSEClient.CONNECTIVITY_CHECK, new HttpHandler() {
+			@Override
+			public void handle(HttpExchange exchange) throws IOException {
+				log.debug("Got connectivity check request");
+				byte[] response = ("ok").getBytes(StandardCharsets.UTF_8);
+				exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+				exchange.getResponseBody().write(response);
+				exchange.getResponseBody().flush();
+				exchange.close();
 			}
 		});
 		return httpServer;
